8e4e4ff1a047acdb05ad67027dfd8ee39b894ee9
Rubens Alves <fa.rubens@gmail.com>
2022-01-28 22:31:33 -0300

    Versao com landing page

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+/* eslint-disable react/jsx-no-target-blank */
+import React from "react";
+import Link from "next/link";
+
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+import Footer from "components/Footers/Footer.js";
+
+export default function Index() {
+  return (
+    <>
+      <IndexNavbar fixed />
+      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+        <div className="container mx-auto items-center flex flex-wrap">
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+            <div className="pt-32 sm:pt-0">
+              <h2 className="font-semibold text-4xl text-blueGray-600">
+               Conheça a Meme Coin e Acompanha o mundo das Cripto Moedas
+              </h2>
+              <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
+               Nosso site está em desenvolvimento, maas caso tenha interesse em conhecer um pouco mais do projeto, entre em contato.
+              </p>
+              <div className="mt-12">
+                <a
+                  href="https://www.memedigital.com.br"
+                  target="_blank"
+                  className="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-400 active:bg-blueGray-500 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
+                >
+                  Entre em contato
+                </a>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860-px"
+          src="/img/image-background.svg"
+          alt="..."
+        />
+      </section>
+
+      <Footer />
+    </>
+  );
+}
